8b442958ebb0d4142a7472a5386ce1eb971c28d2
JunyaoMa <113215266+JunyaoMa@users.noreply.github.com>
2023-10-21 20:25:58 -0700

    Add files via upload

---
+++ StockApp/StockApp/Utils/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  StockApp
+//
+//  Created by Junyao Ma on 10/21/23.
+//
+
+import Foundation
+
+let apiKey = "9f180488d6324cae7852c0ba87523a5a"
+let baseURL = "https://financialmodelingprep.com/api/v3/"
