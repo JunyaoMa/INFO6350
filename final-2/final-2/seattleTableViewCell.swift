b70e372a470aa42c52b15160404a9b3712352b57
JunyaoMa <mjy31093109@gmail.com>
2023-12-09 14:52:32 -0800

    final 2

---
+++ final-2/final-2/seattleTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  seattleTableViewCell.swift
+//  final-2
+//
+//  Created by Junyao Ma on 12/9/23.
+//
+
+import UIKit
+
+class seattleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
